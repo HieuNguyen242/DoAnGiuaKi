ecfe94e12a40794021efb4fc1f3847ebbc6cdf0f
HieuNguyen242 <duchieu242@gmail.com>
2017-04-28 02:27:58 +0700

    LayoutDSSanPham

---
+++ 1460017_DAGK/1460017_DAGK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1460017_DAGK.Startup))]
+namespace _1460017_DAGK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
